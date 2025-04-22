8e8b19743b8613ff3e9a489f63f82074ed2fd6c8
Aram <aram.anto@gmail.com>
2025-04-22 14:42:42 +0200

    popup always rendered inside viewport

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,42 @@
 import styles from "../styles/App.module.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import PopUp from "./PopUp";
+import waldo from "../assets/waldoBackground.jpg";
+
+function normPos(width, pos) {
+  const refWidth = 3000;
+  const ratio = refWidth / width;
+  const newPos = pos.map((x) => x * ratio);
+  return newPos;
+}
 
 function App() {
   const [position, setPosition] = useState([0, 0]);
+  const [cursorPosition, setCursorPosition] = useState([0, 0]);
   const [popUpVisible, setPopUpVisible] = useState(false);
 
   function handleClick(e) {
     setPopUpVisible(true);
-    setPosition([e.clientX, e.clientY]);
+    const image = document.getElementById("waldo");
+    const imgProps = image.getBoundingClientRect();
+    const pos = normPos(imgProps.width, [
+      e.pageX - imgProps.left,
+      // getBoundingClientRect relative to viewport => need to offset scroll
+      e.pageY - (imgProps.top + window.scrollY),
+    ]);
+    setPosition(pos.map((x) => Math.round(x)));
+    setCursorPosition([e.clientX, e.clientY]);
   }
 
   return (
     <>
-      <div className={styles.rootDiv} onClick={handleClick}></div>
-      {popUpVisible && <PopUp pos={position} />}
+      <img
+        id="waldo"
+        className={styles.rootElem}
+        onClick={handleClick}
+        src={waldo}
+      ></img>
+      {popUpVisible && <PopUp pos={position} cursPos={cursorPosition} />}
     </>
   );
 }
--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,17 +1,27 @@
 import styles from "../styles/PopUp.module.css";
+import { useRef, useEffect, useState } from "react";
 
-function PopUp({ pos }) {
+function PopUp({ pos, cursPos }) {
+  const myRef = useRef(null);
   const posi = "X: " + pos[0] + " - Y: " + pos[1];
-  console.log(posi);
+  const [finalPos, setFinalPos] = useState([0, 0]);
+
+  useEffect(() => {
+    const imgData = myRef.current.getBoundingClientRect();
+    const fpx = Math.min(cursPos[0], window.innerWidth - imgData.width);
+    const fpy = Math.min(cursPos[1], window.innerHeight - imgData.height);
+    setFinalPos([fpx, fpy]);
+    console.log(finalPos);
+  }, [cursPos]);
+
   return (
-    <>
-      <div
-        className={styles.main}
-        style={{ top: `${pos[1]}px`, left: `${pos[0]}px` }}
-      >
-        {posi}
-      </div>
-    </>
+    <div
+      ref={myRef}
+      className={styles.main}
+      style={{ top: `${finalPos[1]}px`, left: `${finalPos[0]}px` }}
+    >
+      {posi}
+    </div>
   );
 }
 
